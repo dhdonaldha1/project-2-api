b8ca8d574917666801d4fc059c4f12b21c5399d3
dhdonaldha1 <donaldhas.email@gmail.com>
2020-09-18 00:02:48 -0400

    Added anime route, model, curlscript
    getting unauthorized in the terminal, can sign up/in but cannot create
    anime

---
+++ app/models/anime.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const animeSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  translation: {
+    type: String,
+    required: true
+  },
+  genre: {
+    type: String,
+    required: true
+  },
+  episodes:{
+    type: Number,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Anime', animeSchema)
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 // require route files
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
-
+const animeRoutes= require('./app/routes/anime_routes')
 // require middleware
 const errorHandler = require('./lib/error_handler')
 const replaceToken = require('./lib/replace_token')
@@ -63,7 +63,9 @@ app.use(requestLogger)
 // register route files
 app.use(exampleRoutes)
 app.use(userRoutes)
-
+app.use(animeRoutes)
+//get to respond to hi this is working
+app.get('/', (req, res) => res.send('This is working!!'))
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
 // passed any error messages from them
